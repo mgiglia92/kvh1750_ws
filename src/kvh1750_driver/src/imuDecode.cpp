10a0e40d6ba16a35af5bc0e8c06f8265f3198e10
lizocf <lizelle.ocfemia@cooper.edu>
2023-03-05 22:11:19 -0500

    modify convert_bytes()

--- src/kvh1750_driver/src/imuDecode.cpp
+++ src/kvh1750_driver/src/imuDecode.cpp
@@ -30,21 +30,48 @@ Kvh1750Data imuData;
 uint8_t current[] = { // actually size 36
      0xFE,  0x81,  0xFF,  0x55, // header 
      0x37,  0xA9,  0x6A,  0x6E, // wx
-     0x38,  0x58,  0x6C,  0x1F // wy
+     0x38,  0x58,  0x6C,  0x1F, // wy 
+     0xB7,  0x5B,  0xF8,  0x62, // wz
+     0xBF,  0x80,  0x3E,  0x78, // ax
+     0xBB,  0x65,  0x0D,  0x28, // ay
+     0x3B,  0x0A,  0x37,  0xAC, // az
+     0x77,                      // status
+     0x3D,                      // sequence  
+     0x00,  0x28,               // temperature
+     0x4B,  0xFA,  0x34,  0xD8  // CRC
 };
 
 // uint32_t data1 = 0x37A96A6E;
 // uint32_t data2 = 0x38586C1F;
 
 void convert_bytes(Kvh1750Data* imudata, uint8_t *bytearray, size_t len){
-uint32_t Wx;
+uint32_t Wx, Wy, Wz, Ax, Ay, Az;
 for (int i = 0; i<4; i++){
-    uint32_t temp = ((uint32_t) current[4 + i]) << (24 - 8*i);
-    Wx |= temp;
+    uint32_t temp1 = ((uint32_t) current[4 + i]) << (24 - 8*i);
+    Wx |= temp1;
+    uint32_t temp2 = ((uint32_t) current[8 + i]) << (24 - 8*i);
+    Wy |= temp2;
+    uint32_t temp3 = ((uint32_t) current[12 + i]) << (24 - 8*i);
+    Wz |= temp3;
+    uint32_t temp4 = ((uint32_t) current[16 + i]) << (24 - 8*i);
+    Ax |= temp4;
+    uint32_t temp5 = ((uint32_t) current[20 + i]) << (24 - 8*i);
+    Ay |= temp5;
+    uint32_t temp6 = ((uint32_t) current[24 + i]) << (24 - 8*i); // lmao
+    Az |= temp6; 
     } 
 memcpy(&imuData.wx, &Wx, sizeof(imuData.wx));
 cout << imuData.wx << endl;
-
+memcpy(&imuData.wy, &Wy, sizeof(imuData.wy));
+cout << imuData.wy << endl;
+memcpy(&imuData.wz, &Wz, sizeof(imuData.wz));
+cout << imuData.wz << endl;
+memcpy(&imuData.ax, &Ax, sizeof(imuData.ax));
+cout << imuData.ax << endl;
+memcpy(&imuData.ay, &Ay, sizeof(imuData.ay));
+cout << imuData.ay << endl;
+memcpy(&imuData.az, &Az, sizeof(imuData.az));
+cout << imuData.az << endl;
 }
 
 // 
