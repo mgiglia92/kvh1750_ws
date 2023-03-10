27213fecff5fa2d9ff938d0b1cd2955bd7b0f63a
Giuseppe Quaratino <giuseppe.quaratino@cooper.edu>
2023-03-10 12:10:52 -0500

    imu decodes properly, no CRC check implemented

--- src/kvh1750_driver/src/imuDecode.cpp
+++ src/kvh1750_driver/src/imuDecode.cpp
@@ -25,8 +25,6 @@ struct Kvh1750Data
     int16_t temp;
 };
 
-Kvh1750Data imuData;
-
 uint8_t current[] = { // actually size 36
      0xFE,  0x81,  0xFF,  0x55, // header 
      0x37,  0xA9,  0x6A,  0x6E, // wx
@@ -41,63 +39,90 @@ uint8_t current[] = { // actually size 36
      0x4B,  0xFA,  0x34,  0xD8  // CRC
 };
 
-// uint32_t data1 = 0x37A96A6E;
-// uint32_t data2 = 0x38586C1F;
-
-void convert_bytes(Kvh1750Data* imudata, uint8_t *bytearray, size_t len){
-uint32_t Wx=0, Wy=0, Wz=0, Ax=0, Ay=0, Az=0;
-uint8_t status=0, seq=0;
-int16_t temp=0;
-
-// for (int i = 0; i<4; i++){
-//     Wx |= ((uint32_t) current[4 + i]) << (24 - 8*i);
-//     Wy |= ((uint32_t) current[8 + i]) << (24 - 8*i);
-//     Wz |= ((uint32_t) current[12 + i]) << (24 - 8*i);
-//     Ax |= ((uint32_t) current[16 + i]) << (24 - 8*i);
-//     Ay |= ((uint32_t) current[20 + i]) << (24 - 8*i);
-//     Az |= ((uint32_t) current[24 + i]) << (24 - 8*i); 
-// } 
-
-    Wx = ((uint32_t) current[4]) << 24 | ((uint32_t) current[5]) << 16 | ((uint32_t) current[6]) << 8 | ((uint32_t) current[7]);
-    Wy = ((uint32_t) current[8]) << 24 | ((uint32_t) current[9]) << 16 | ((uint32_t) current[10]) << 8 | ((uint32_t) current[11]);
-    Wz = ((uint32_t) current[12]) << 24 | ((uint32_t) current[13]) << 16 | ((uint32_t) current[14]) << 8 | ((uint32_t) current[15]);
-
-    Ax = ((uint32_t) current[16]) << 24 | ((uint32_t) current[17]) << 16 | ((uint32_t) current[18]) << 8 | ((uint32_t) current[19]);
-    Ay = ((uint32_t) current[20]) << 24 | ((uint32_t) current[21]) << 16 | ((uint32_t) current[22]) << 8 | ((uint32_t) current[23]);
-    Az = ((uint32_t) current[24]) << 24 | ((uint32_t) current[25]) << 16 | ((uint32_t) current[26]) << 8 | ((uint32_t) current[27]);
-
-    status = (uint8_t) current[28];
-    seq = (uint8_t) current[29];
+// Returns 1 if the message is valid and 0 if the data is corrupted
+bool status_check(Kvh1750Data* imuData, uint8_t *bytearray){
     
-    temp = ((uint16_t) current[30]) << 8 | ((uint16_t) current[31]);
+    uint8_t health;
 
-memcpy(&imuData.wx, &Wx, sizeof(imuData.wx));
-cout << imuData.wx << endl;
-memcpy(&imuData.wy, &Wy, sizeof(imuData.wy));
-cout << imuData.wy << endl;
-memcpy(&imuData.wz, &Wz, sizeof(imuData.wz));
-cout << imuData.wz << endl;
+    imuData->status = (uint8_t) bytearray[28];
+    cout << "Message Status: "<< (uint32_t)imuData->status << endl; // should be hex
+    
+    health = imuData->status ^ 0x77;
+    if(health != 0){
+        cout << "DATA INVALID: Data is corrupted" << endl;  // modify to show which message is corrupted i.e. Gyro X, Acceleroemter Y, etc.
+        return 0;
+    }
 
-memcpy(&imuData.ax, &Ax, sizeof(imuData.ax));
-cout << imuData.ax << endl;
-memcpy(&imuData.ay, &Ay, sizeof(imuData.ay));
-cout << imuData.ay << endl;
-memcpy(&imuData.az, &Az, sizeof(imuData.az));
-cout << imuData.az << endl;
-
-imuData.status = status;
-cout << (uint32_t)imuData.status << endl;
-imuData.sequence = seq;
-cout << (uint32_t)imuData.sequence << endl;
-imuData.temp = temp;
-cout << imuData.temp << endl;
-
+    return 1;
 }
 
+// Returns 0 if message counter is greater than previous
+// Returns -1 otherwise as data is invalid
+int sequence_check(Kvh1750Data* imuData, uint8_t *bytearray, uint8_t prev_seq){
+    
+    imuData->sequence  = (uint8_t) bytearray[29];
+    cout << "Message Counter: "<< (uint32_t)imuData->sequence << endl; // Wrong number I think the manual is wrong, either way we just need to verify tha tconsecutive mesages increment the counter
+
+    if(imuData->sequence > prev_seq || imuData->sequence == 0){
+        return 0;
+    }
+    
+    cout << "DATA INVALID: Incorrect counter" << endl;
+    return -1;
+}
+
+// Generate and compare checksum using generator polynomial
+// void CRC_check(Kvh1750Data* imuData, uint8_t *bytearray){
+// 
+//     uint32_t polynomial = 0x04C11DB7;
+//
+// }
+
+void convert_bytes(Kvh1750Data* imuData, uint8_t *bytearray){
+    
+    uint32_t Wx=0, Wy=0, Wz=0, Ax=0, Ay=0, Az=0;
+    int16_t temp=0;
+
+    Wx = ((uint32_t) bytearray[4]) << 24 | ((uint32_t) bytearray[5]) << 16 | ((uint32_t) bytearray[6]) << 8 | ((uint32_t) bytearray[7]);
+    Wy = ((uint32_t) bytearray[8]) << 24 | ((uint32_t) bytearray[9]) << 16 | ((uint32_t) bytearray[10]) << 8 | ((uint32_t) bytearray[11]);
+    Wz = ((uint32_t) bytearray[12]) << 24 | ((uint32_t) bytearray[13]) << 16 | ((uint32_t) bytearray[14]) << 8 | ((uint32_t) bytearray[15]);
+
+    memcpy(&imuData->wx, &Wx, sizeof(imuData->wx));
+    memcpy(&imuData->wy, &Wy, sizeof(imuData->wy));
+    memcpy(&imuData->wz, &Wz, sizeof(imuData->wz));
+
+    Ax = ((uint32_t) bytearray[16]) << 24 | ((uint32_t) bytearray[17]) << 16 | ((uint32_t) bytearray[18]) << 8 | ((uint32_t) bytearray[19]);
+    Ay = ((uint32_t) bytearray[20]) << 24 | ((uint32_t) bytearray[21]) << 16 | ((uint32_t) bytearray[22]) << 8 | ((uint32_t) bytearray[23]);
+    Az = ((uint32_t) bytearray[24]) << 24 | ((uint32_t) bytearray[25]) << 16 | ((uint32_t) bytearray[26]) << 8 | ((uint32_t) bytearray[27]);
+
+    memcpy(&imuData->ax, &Ax, sizeof(imuData->ax));
+    memcpy(&imuData->ay, &Ay, sizeof(imuData->ay));
+    memcpy(&imuData->az, &Az, sizeof(imuData->az));
+
+    //Convert from g to m/s^2
+    imuData->ax = (imuData->ax)/10;
+    imuData->ay = (imuData->ay)/10;
+    imuData->az = (imuData->az)/10;
+
+    imuData->temp = ((uint16_t) bytearray[30]) << 8 | ((uint16_t) bytearray[31]);
+
+    cout << "Gyro X: " << imuData->wx << endl;
+    cout << "Gyro Y: " << imuData->wy << endl;
+    cout << "Gyro Z: " << imuData->wz << endl;
+
+    cout << "Accel X: " << imuData->ax << endl;
+    cout << "Accel Y: " << imuData->ay << endl;
+    cout << "Accel Z: " << imuData->az << endl;
+
+    cout << "Temp: " << imuData->temp << endl;
+
+}
 
 int main(){
 
-    convert_bytes(&imuData, current, sizeof(current));
+    Kvh1750Data imuData;
 
+    if(status_check(&imuData, current))
+        convert_bytes(&imuData, current);
+    
 }
-
